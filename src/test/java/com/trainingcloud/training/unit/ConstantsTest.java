aa8e3e25f6556944f917a262e44dc9881fddce75
Anibal Higueros <ahigueros@xumak.com>
2022-08-03 15:39:27 -0600

    [fix] fix test constants not empty

--- src/test/java/com/trainingcloud/training/unit/ConstantsTest.java
+++ src/test/java/com/trainingcloud/training/unit/ConstantsTest.java
@@ -5,18 +5,15 @@ import jdk.jfr.Description;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class ConstantsTest {
 
-    Constants constants;
-
     @Test
     @Description("Constants - Evaluate have not empty value")
     void constantsNotEmptyValue() {
-        Assertions.assertNotNull(constants.DATE_FORMAT);
+        Assertions.assertNotNull(Constants.DATE_FORMAT);
     }
 
 }
