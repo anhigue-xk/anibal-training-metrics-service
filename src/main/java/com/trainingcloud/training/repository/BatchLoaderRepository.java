49849a8a71eade03e2a9cc021fc8dd4267a79073
Anibal Higueros <ahigueros@xumak.com>
2022-08-04 10:58:37 -0600

    [update] include find by timestamp between method

--- src/main/java/com/trainingcloud/training/repository/BatchLoaderRepository.java
+++ src/main/java/com/trainingcloud/training/repository/BatchLoaderRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.trainingcloud.training.Entity.BatchLoader;
 
+import java.util.Date;
+import java.util.List;
+
 public interface BatchLoaderRepository extends CrudRepository<BatchLoader, Long>{
-    
+    List<BatchLoader> findByTimestampBetween(Date start, Date end);
 }
